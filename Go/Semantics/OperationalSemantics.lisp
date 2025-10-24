cfac675d455d8026a8a11754b9ebcdc653d2dabf
Aleksandr <79756667+AlKharkov@users.noreply.github.com>
2025-10-24 21:50:00 +0700

    Update OperationalSemantics.lisp

--- Go/Semantics/OperationalSemantics.lisp
+++ Go/Semantics/OperationalSemantics.lisp
@@ -1,23 +1,30 @@
 #|
 	Operational semantics for the Go language
 
-    Last edit: 17/10/2025
+    Last edit: 24/10/2025
 |#
 
 
+;;; Enviroment and Agents
 (mot "env" 
     :at "agents" (listt "agent")
-    :at "init target construct" (uniont "expression" "statement" "external declaration")
+    :at "init target construct" (uniont (listt "external declaration"))
+    ;:at "init target construct" (uniont "expression" "statement" "external declaration")  ; Зачем это?
 )
-
 
 (mot "agent"
-    :at "variable location" (cobject "variable" "location")
-    :at "location value" (cobject "location" "Go value")
-    :at "location type" (cobject "location" "type")
+    :at "variable location" (cot :amap "variable" "location")
+    :at "location value" (cot :amap "location" "Go value")
+    :at "location type" (cot :amap "location" "type")
 )
 
 
-;;; Auxiliary generic objects
-(mot "location")
-(mot "Go value" (uniont "constant" "array" "slice" "struct" "function" "interface" "map" "expression" "statement"))
+;;; Go values
+(aclosure "opsem" "Go value" i ac i)
+
+
+;;; Variables
+(aclosure "opsem" "variable" i ac (match :ap ac "agent" a :do (aget a "location value" (aget a "location" i))))  ; Что после :do происходит?
+
+
+;;; Expressions
