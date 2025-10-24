59b21ac9282747aaeee5a746cfed669e79a169a8
Aleksandr <79756667+AlKharkov@users.noreply.github.com>
2025-10-24 21:49:44 +0700

    Update Model.lisp

--- Go/Semantics/Model.lisp
+++ Go/Semantics/Model.lisp
@@ -1,7 +1,7 @@
 #|
 	Model of the Go language
 
-	Last edit: 22/10/2025
+	Last edit: 24/10/2025
 |#
 
 
@@ -19,7 +19,7 @@
 
 
 ;;; Variables
-(typedef "variable" (uniont "identifier"))
+(mot "variable" :at "value" "identifier")
 
 
 ;;; Types
@@ -122,7 +122,7 @@
 ; 	Polar    = polar      // Polar and polar denote identical types
 ; )
 ; type set[P comparable] = map[P]bool
-(mot "type def" :at "identifier" "identifier" :at "type parameters" "type parameters" :at "type" "type")
+(mot "type def" :at "name" "identifier" :at "type parameters" "type parameters" :at "type" "type")
 ; type (
 ; 	Point struct{ x, y float64 }    // Point and struct{ x, y float64 } are different types
 ; 	polar Point                     // polar and Point denote different types
@@ -132,18 +132,18 @@
 ; 	value        any
 ; }
 (mot "type parameters" :at "param declarations" (listt "type param decl"))
-(mot "type param decl" :at "identifiers" (listt "identifier") :at "type constraint" "type constraint")
+(mot "type param decl" :at "names" (listt "identifier") :at "type constraint" "type constraint")
 (mot "type constraint" :at "constraints" "type elem")  ; [T1 comparable, T2 any]
 ;;var declarations
 (mot "var decl" :at "specifiers" (listt "var spec"))
-(mot "var spec" :at "identifiers" (listt "identifier") :at "type" "type" :at "expressions" (listt "expression"))
+(mot "var spec" :at "names" (listt "identifier") :at "type" "type" :at "expressions" (listt "expression"))
 ; var U, V, W float64
 ; var k = 0
 ; var x, y float32 = -1, -2
-(mot "short var decl" :at "identifiers" (listt "identifier") :at "expressions" (listt "expression"))
+(mot "short var decl" :at "names" (listt "identifier") :at "expressions" (listt "expression"))
 ; f := func() int { return 7 }
 ;;function declarations
-(mot "function decl" :at "identifier" "identifier" :at "type parameters" "type parameters" :at "signature" "signature" :at "body" "function body")
+(mot "function decl" :at "name" "identifier" :at "type parameters" "type parameters" :at "signature" "signature" :at "body" "function body")
 (mot "function body" :at "block" "block")
 ; func min[T ~int|~float64](x, y T) T {
 ; 	if x < y {
@@ -165,19 +165,14 @@
 (typedef "literal" (uniont "basic literal" "composite literal" "function literal"))
 (typedef "basic literal" (uniont "constant"))
 (mot "composite literal" :at "type" "literal type" :at "value" (listt "keyed element"))
-(typedef "literal type" (uniont "struct type" "array type" "*array type" "slice type" "map type" "short arr type"))
-(mot "... type" :at "type" "type")  ; ... ElementType
-(mot "short arr type" :at "name" (uniont "identifier" "qualified identifier") :at "type args" (listt "type"))
-(mot "qualified identifier" :at "package name" "identifier" :at "identifier" "identifier")  ; math.Sin
 (mot "keyed element" :at "key" "key" :at "element" "element")
+(typedef "literal type" (uniont "struct type" "array type" "slice type" "map type" "short arr type"))
+(mot "short arr type" :at "name" (uniont "identifier" "qualified identifier") :at "type args" (listt "type"))
+(mot "qualified identifier" :at "package name" "identifier" :at "name" "identifier")  ; math.Sin
 (typedef "key" (uniont "field name" "expression" (listt "keyed element")))
 (typedef "element" (uniont "expression" (listt "keyed element")))
 (mot "function literal" :at "signature" "signature" :at "body" "block")  ; func(a, b int, z float64) bool { return a*b < int(z) }
 (mot "parenthesized expression" :at "expression" "expression")
-; import (
-;   "fmt"
-;   "math"
-; )
 ;;primary expressions
 (typedef "primary expression" (uniont "operand" "conversion" "method expr" "primary expr & selector" "primary expr & index expr" "primary expr & slice expr" "primary expr & type assertion" "primary expr & argument"))
 (mot "conversion" :at "type" "type" :at "expression" "expression")  ; (*Point)(p)    // p is converted to *Point
@@ -196,15 +191,12 @@
 (mot "type assertion" :at "type" "type")  ; .(Type)
 ; var x interface{} = 7    // x has dynamic type int and value 7
 ; i := x.(int)             // i has type int and value 7
-(mot "primary expr & arguments" :at "primary expr" "primary expression" :at "arguments" (listt "expression"))  ; math.Sin(2)
+(mot "primary expr & arguments" :at "primary expr" "primary expression" :at "arguments" (listt "argument"))  ; math.Sin(2)
+(mot "argument" :at "expressions" (listt "expression") :at "type" "type")
 ;;expressions
-(typedef "expression" (uniont "unary expression" "1 & binary operator & 2"))
-(mot "1 & binary operator & 2" :at 1 "expression" :at "binary operator" "binary operator" :at 2 "expression")
-(typedef "unary expression" (uniont "primary expression" "unary operator & unary expression"))
-(mot "unary operator & unary expression" :at "operator" "unary operator" :at "expression" "unary expression")
-;;operators
-;;unary operators
-(typedef "unary operator" (uniont "+1" "-1" "^1" "!1" "*1" "&1" "<-"))
+(typedef "expression" (uniont "unary expression" "binary expression"))
+;;unary expressions
+(typedef "unary expression" (uniont "primary expression" "+1" "-1" "^1" "!1" "*1" "&1" "<-"))
 (mot "+1" :at 1 "expression")  ; +x == x
 (mot "-1" :at 1 "expression")  ; -x == -1 * x
 (mot "^1" :at 1 "expression")  ; ^10(2) == 01(2)
@@ -212,12 +204,12 @@
 (mot "*1" :at 1 "expression")
 (mot "&1" :at 1 "expression")
 (mot "<-" :at "channel" "channel type")
-;;binary operators
-(typedef "binary operator" (uniont "1||2" "1&&2" "rel operator" "add operator" "mul operator"))
+;;binary expressions
+(typedef "binary expression" (uniont "1||2" "1&&2" "rel expression" "add expression" "mul expression"))
 (mot "1||2" :at 1 "expression" :at 2 "expression")
 (mot "1&&2" :at 1 "expression" :at 2 "expression")
-;;binary multiplication operators
-(typedef "mul operator" (uniont "1*2" "1/2" "1%2" "1<<2" "1>>2" "1&2" "1&^2"))
+;;binary multiplication expressions
+(typedef "mul expression" (uniont "1*2" "1/2" "1%2" "1<<2" "1>>2" "1&2" "1&^2"))
 (mot "1*2" :at 1 "expression" :at 2 "expression")
 (mot "1/2" :at 1 "expression" :at 2 "expression")
 (mot "1%2" :at 1 "expression" :at 2 "expression")
@@ -225,17 +217,17 @@
 (mot "1>>2" :at 1 "expression" :at 2 "expression")
 (mot "1&2" :at 1 "expression" :at 2 "expression")
 (mot "1&^2" :at 1 "expression" :at 2 "expression") ; 11001010(2) &^ 10101100(2) == 01000010(2)    // 1 <=> 1 &^ 0
-;;binary addition operators
-(typedef "add operator" (uniont "1+2" "1-2" "1|2" "1^2"))
+;;binary addition expressions
+(typedef "add expression" (uniont "1+2" "1-2" "1|2" "1^2"))
 (mot "1+2" :at 1 "expression" :at 2 "expression")
 (mot "1-2" :at 1 "expression" :at 2 "expression")
 (mot "1|2" :at 1 "expression" :at 2 "expression")
 (mot "1^2" :at 1 "expression" :at 2 "expression")
-;;relation operators
-(typedef "rel operator" (uniont "1=2" "1!2" "unequality operator"))
+;;relation expressions
+(typedef "rel expression" (uniont "1=2" "1!2" "unequality expression"))
 (mot "1==2" :at 1 "expression" :at 2 "expression")
 (mot "1!=2" :at 1 "expression" :at 2 "expression")
-(typedef "unequality operator" (uniont "1<2" "1<2" "1>2" "1>2"))
+(typedef "unequality expression" (uniont "1<2" "1<2" "1>2" "1>2"))
 (mot "1<2" :at 1 "expression" :at 2 "expression")
 (mot "1<=2" :at 1 "expression" :at 2 "expression")
 (mot "1>2" :at 1 "expression" :at 2 "expression")
@@ -244,7 +236,7 @@
 ;;; Statements
 (typedef "statement" (uniont "declaration" "label stmt" "simple stmt" "go stmt" "return stmt" "break stmt" "continue stmt" "goto stmt" "fallthrough stmt" "block" "if stmt" "switch stmt" "select stmt" "for stmt" "defer stmt"))
 (typedef "simple statement" (uniont "empty stmt" "expression stmt" "send stmt" "1++" "1--" "assignment stmt" "short var decl"))
-(typedef "empty stmt")  ; <doc> The empty statement does nothing
+(typedef "empty stmt")  ; The empty statement does nothing
 (mot "label stmt" :at "label" "label" :at "statement" "statement")
 (mot "label" :at "name" "identifier")
 ; "label": "statement"
@@ -270,44 +262,47 @@
 (mot "1&=2" :at 1 (listt "expression") :at 2 (listt "expression"))
 (mot "1&^=2" :at 1 (listt "expression") :at 2 (listt "expression"))
 ;;if statement
-(mot "if stmt" :at "init" "simple stmt" :at "condition" "expression" :at "then" "block" :at "else" (uniont "if statement" (listt "statement")))
+(mot "if stmt" :at "init" "simple stmt" :at "condition" "expression" :at "then" "block" :at "else" (uniont "if statement" "block"))
 ; if x := f(); x < y {
 ; 	return x
 ; } else if x > z {
 ; 	return z
 ; }
+;;switch statement
 (typedef "switch stmt" (uniont "expr switch stmt" "type switch stmt"))
 (mot "expr switch stmt" :at "init" "simple stmt" :at "controlling expression" "expression" :at "cases" (listt "expr case clause"))
-(mot "expr case clause" :at "cases" (listt "expression") :at "statements" (listt "statement"))
+(mot "expr case clause" :at "cases" (uniont (listt "expression") "default") :at "statements" (listt "statement"))
+(mot "default")
 ; switch tag {
 ; case 0, 1, 2, 3: s1()
 ; case 4, 5, 6, 7: s2()
 ; default: s3()
 ; }
 (mot "type switch stmt" :at "init" "simple stmt" :at "guard" "type switch guard" :at "cases" (listt "type case clause"))
-(mot "type switch guard" :at "type variable" "identifier" :at "variable" "primary expression")
-(mot "type case clause" :at "cases" (listt "type") :at "statements" (listt "statement"))
+(mot "type switch guard" :at "name" "identifier" :at "variable" "primary expression")
+(mot "type case clause" :at "cases" (uniont (listt "type") "default") :at "statements" (listt "statement"))
 ; switch t := x.(type) {              // type checker
 ; case nil:
 ; 	fmt.Println("x is nil")  
 ; default:
 ;   fmt.Println("don't know the type")
+;;for statement
 (typedef "for statement" (uniont "for stmt with condition clause" "for stmt with range clause"))
 (mot "for stmt with condition clause" :at "init" "simple stmt" :at "condition" "expression" :at "post" "simple stmt" :at "body" "block")
-(mot "for stmt with range clause" :at "location" (uniont (listt "expression") (listt "identifier")) :at "expression" "expression" :at "body" "block")
+(mot "for stmt with range clause" :at "names" (uniont (listt "expression") (listt "identifier")) :at "expression" "expression" :at "body" "block")
 ; for x < y { a *= 2 }                // the usual while loop
 ; for i := 0; i < 10; i++ { f(i) }    // the usual for loop
 ; a := [5]int{1, 2, 3, 4, 5}
 ; for i, v := range a {
 ;   fmt.Println(i, v)                 // i a[i]
 ; }
-(mot "go stmt" :at "thread body" "expression")  ; <doc> It starts the execution of a function call as an independent concurrent thread of control
+(mot "go stmt" :at "body" "expression")  ; It starts the execution of a function call as an independent concurrent thread of control
 ; go Server()
 ; go func(ch chan<- bool) { for { sleep(10); ch <- true }} (c)
 (mot "select stmt" :at "statement" (listt "common clause"))
 (mot "common clause" :at "case" "common case" :at "statements" (listt "statement"))
-(typedef "common case" (uniont "send stmt" "recive stmt"))
-(mot "recive stmt" :at "list" (uniont (listt "expression") (listt "identifier")) :at "expression" "expression")
+(typedef "common case" (uniont "send stmt" "recive stmt" "default"))
+(mot "recive stmt" :at "names" (uniont (listt "expression") (listt "identifier")) :at "expression" "expression")
 ; func f1(c1 chan int) {
 ; 	a := <-c1
 ; 	c1 <- a
@@ -351,16 +346,19 @@
 ; }
 
 
-;;; Source file organization
-(mot "source file" :at "package" "identifier" :at "import decl" (listt "import package") :at "declarations" "top level decl")
+;;; Packages
+(mot "source file" :at "package name" "identifier" :at "import decl" (listt "import package") :at "declarations" (listt "top level decl"))
 ;;import declaration
-(mot "import package" :at "name" "package name" :at "path" "path")
+(mot "import package" :at "package name" "identifier" :at "path" "path")
 (typedef "path" (uniont string))  ; import m "lib/math"
 
 
-;;; Higher level constructs
-(mot "translation unit" :at "declarations" (listt "external declaration"))
-(typedef "external declaration" (uniont "function definition" "declaration"))
-(mot "function definition"
-	:at ""  ; TODO
-)
+;;; External definitions
+;(mot "translation unit" :at "declarations" (listt "external declaration"))  	; Что это и зачем?
+;(typedef "external declaration" (uniont "function definition" "declaration"))  ; Что это и зачем?
+(mot "function definition" :at "signature" "signature" :at "body" "block")
+
+
+;;; Semantics constructs
+(mot "location")
+(typedef "go value" (uniont "constant" "location" "function definition"))
